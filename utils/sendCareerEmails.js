c5ce4029ee747332862f54c83e542c334a9dcbaf
DilpreetSinghWeb <dilpreetweb65@gmail.com>
2025-05-18 21:10:41 +0530

    changed hostinger

--- utils/sendCareerEmails.js
+++ utils/sendCareerEmails.js
@@ -2,14 +2,16 @@ import nodemailer from "nodemailer";
 import generateCareerAdminEmail from "../emailsTemplates/careers/toAdmin.js";
 import generateCareerClientEmail from "../emailsTemplates/careers/toClient.js";
 
-// Create Nodemailer transport using SendGrid
 const transporter = nodemailer.createTransport({
-  service: "SendGrid",
+  host: "smtp.hostinger.com",
+  port: 465,
+  secure: true,
   auth: {
-    user: "apikey", 
-    pass: process.env.SENDGRID_API_KEY, 
+    user: process.env.HOSTINGER_EMAIL,
+    pass: process.env.HOSTINGER_PASSWORD,
   },
 });
+console.log(process.env.HOSTINGER_PASSWORD)
 
 export default async function sendCareerEmails(data) {
   const adminMail = generateCareerAdminEmail(data);
@@ -17,14 +19,14 @@ export default async function sendCareerEmails(data) {
 
   try {
     await transporter.sendMail(adminMail);
-    
+    console.log("Mail sent to admin");
   } catch (err) {
     console.error("Error sending mail to admin:", err);
   }
 
   try {
     await transporter.sendMail(clientMail);
-   
+    console.log("Mail sent to client");
   } catch (err) {
     console.error("Error sending mail to client:", err);
   }
